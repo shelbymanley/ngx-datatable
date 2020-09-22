998f41d74924541e52788297598dde70703758f1
Mark Berry <markberry867@gmail.com>
2020-09-22 17:16:01 -0500

    Update scroller tests

--- projects/swimlane/ngx-datatable/src/lib/components/body/scroller.component.spec.ts
+++ projects/swimlane/ngx-datatable/src/lib/components/body/scroller.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+
+import { ScrollbarHelper } from './../../services/scrollbar-helper.service';
 import { ScrollerComponent } from './scroller.component';
 
 describe('ScrollerComponent', () => {
@@ -9,7 +11,17 @@ describe('ScrollerComponent', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ScrollerComponent]
+      declarations: [ScrollerComponent],
+      providers: [
+        {
+          provide: ScrollbarHelper,
+          useValue: {
+            onInitScroller: jasmine.createSpy('onInitScroller'),
+            onDestroyScroller: jasmine.createSpy('onDestroyScroller'),
+            setOffset: jasmine.createSpy('setOffset')
+          }
+        }
+      ]
     });
   });
 
@@ -24,6 +36,42 @@ describe('ScrollerComponent', () => {
   describe('fixture', () => {
     it('should have a component instance', () => {
       expect(component).toBeTruthy();
+    });
+  });
+
+  describe('no scrollbar', () => {
+    beforeEach(() => {
+      component.scrollbarV = false;
+      component.scrollbarH = false;
+      fixture.detectChanges();
+    });
+
+    it('should not init scroller', () => {
+      expect(TestBed.inject(ScrollbarHelper).onInitScroller).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('vertical scrollbar', () => {
+    beforeEach(() => {
+      component.scrollbarV = true;
+      component.scrollbarH = false;
+      fixture.detectChanges();
+    });
+
+    it('should init scroller', () => {
+      expect(TestBed.inject(ScrollbarHelper).onInitScroller).toHaveBeenCalled();
+    });
+  });
+
+  describe('horizontal scrollbar', () => {
+    beforeEach(() => {
+      component.scrollbarV = false;
+      component.scrollbarH = true;
+      fixture.detectChanges();
+    });
+
+    it('should not init scroller', () => {
+      expect(TestBed.inject(ScrollbarHelper).onInitScroller).toHaveBeenCalled();
     });
   });
 });
