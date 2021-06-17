0642c11e4c4b3dcc87b948aab128c96efbf7e9e5
Shelby Manley <shelby.manley@theseam.com>
2021-06-17 11:41:21 -0500

    filter out disabled rows when onHeaderSelect is clicked

--- projects/swimlane/ngx-datatable/src/lib/components/datatable.component.ts
+++ projects/swimlane/ngx-datatable/src/lib/components/datatable.component.ts
@@ -1091,23 +1091,34 @@ export class DatatableComponent implements OnInit, DoCheck, AfterViewInit {
       // before we splice, chk if we currently have all selected
       const first = this.bodyComponent.indexes.first;
       const last = this.bodyComponent.indexes.last;
-      const allSelected = this.selected.length === last - first;
+
+      let selectableRows = this._internalRows.slice(first, last);
+      if (this.displayCheck) {
+        selectableRows = selectableRows.filter(r => this.displayCheck(r));
+      }
+      const allSelected = this.selected.length === selectableRows.length;
 
       // remove all existing either way
       this.selected = [];
 
       // do the opposite here
       if (!allSelected) {
-        this.selected.push(...this._internalRows.slice(first, last));
+        this.selected.push(...selectableRows);
       }
     } else {
       // before we splice, chk if we currently have all selected
-      const allSelected = this.selected.length === this.rows.length;
+      let selectableRows = this.rows;
+      if (this.displayCheck) {
+        selectableRows = selectableRows.filter(r => this.displayCheck(r));
+      }
+      const allSelected = this.selected.length === selectableRows.length;
+
       // remove all existing either way
       this.selected = [];
+
       // do the opposite here
       if (!allSelected) {
-        this.selected.push(...this.rows);
+        this.selected.push(...selectableRows);
       }
     }
 
